b13377cd7890d944a61debfdc78b4f3c2931d7cc
wuweiweiwu <wuxx1045@umn.edu>
2018-02-10 16:52:05 -0800

    File menu done

--- app/components/MenuBar/Menu.js
+++ app/components/MenuBar/Menu.js
@@ -1,9 +1,82 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 import styles from './Menu.css';
 
 export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.togglePopup = this.togglePopup.bind(this);
+  }
+
+  togglePopup() {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }));
+  }
+
   render() {
-    return <div className={styles.menu}>File</div>;
+    const { title, items } = this.props;
+    const { open } = this.state;
+
+    const firstPart = str => str.split('&')[0];
+
+    const hotkey = str => {
+      const index = str.indexOf('&');
+      if (index >= 0) {
+        return str[index + 1];
+      }
+      return null;
+    };
+
+    const secondPart = str => {
+      const split = str.split('&')[1];
+      if (!split || split.length === 0) {
+        return '';
+      }
+      return split.substring(1);
+    };
+
+    return (
+      <div className={styles.menu__container}>
+        <div
+          className={classNames({
+            [styles.menu__button]: true,
+            [styles['menu__button--active']]: open
+          })}
+          onClick={this.togglePopup}
+        >
+          {title}
+        </div>
+        <div
+          className={classNames({
+            [styles.menu__popup]: true,
+            [styles['menu__popup--disabled']]: !open
+          })}
+        >
+          <table className={styles.menu__popup__table}>
+            <tbody>
+              {items.map(item => (
+                <tr className={styles.menu__item}>
+                  <td className={styles.menu__item__checkbox} />
+                  <td className={styles.menu__item__label}>
+                    {firstPart(item.item)}
+                    {hotkey(item.item) && (
+                      <span className={styles.menu__hotkey}>{hotkey(item.item)}</span>
+                    )}
+                    {secondPart(item.item)}
+                  </td>
+                  <td className={styles.menu__item__shortcut}>{item.shortcut}</td>
+                  <td className={styles.menu__item__submenu} />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
 }
--- app/components/MenuBar/MenuBar.js
+++ app/components/MenuBar/MenuBar.js
@@ -6,9 +6,106 @@ import styles from './MenuBar.css';
 
 export default class MenuBar extends Component {
   render() {
+    const fileItems = [
+      {
+        item: '&New',
+        shortcut: 'Ctrl+N',
+        // action: file_new,
+        description: 'Creates a new document.'
+      },
+      {
+        item: '&Open',
+        shortcut: 'Ctrl+O',
+        // action: file_open,
+        description: 'Opens an existing document.'
+      },
+      {
+        item: '&Save',
+        shortcut: 'Ctrl+S',
+        // action: file_save,
+        description: 'Saves the active document.'
+      },
+      {
+        item: 'Save &As',
+        shortcut: 'Ctrl+Shift+S',
+        // in mspaint, no shortcut is listed, but it supports F12; it doesn't support Ctrl+Shift+S
+        // action: file_save_as,
+        description: 'Saves the active document with a new name.'
+      },
+      // $MenuBar.DIVIDER,
+      {
+        item: '&Load From URL',
+        // shortcut: "Ctrl+L",
+        // action: file_load_from_url,
+        description: 'Opens an image from the web.'
+      },
+      {
+        item: '&Upload To Imgur',
+        // action: upload_to_imgur,
+        description: 'Uploads the active document to Imgur'
+      },
+      // $MenuBar.DIVIDER,
+      {
+        item: 'Manage Storage',
+        // action: manage_storage,
+        description: 'Manages storage of previously created or opened pictures.'
+      },
+      // $MenuBar.DIVIDER,
+      {
+        item: 'Print Pre&view',
+        // action() {
+        //   print();
+        // },
+        description: 'Prints the active document and sets printing options.'
+        // description: "Displays full pages.",
+      },
+      {
+        item: 'Page Se&tup',
+        // action() {
+        //   print();
+        // },
+        description: 'Prints the active document and sets printing options.'
+        // description: "Changes the page layout.",
+      },
+      {
+        item: '&Print',
+        shortcut: 'Ctrl+P',
+        // action() {
+        //   print();
+        // },
+        description: 'Prints the active document and sets printing options.'
+      },
+      // $MenuBar.DIVIDER,
+      {
+        item: 'Set As &Wallpaper (Tiled)',
+        // action: set_as_wallpaper_tiled,
+        description: 'Tiles this bitmap as the desktop background.'
+      },
+      {
+        item: 'Set As Wallpaper (&Centered)', // in mspaint it's Wa&llpaper
+        // action: set_as_wallpaper_centered,
+        description: 'Centers this bitmap as the desktop background.'
+      },
+      // $MenuBar.DIVIDER,
+      {
+        item: 'Recent File',
+        enabled: false, // @TODO for chrome app / desktop app
+        description: ''
+      },
+      // $MenuBar.DIVIDER,
+      {
+        item: 'E&xit',
+        shortcut: 'Alt+F4',
+        // action() {
+        //   close();
+        // },
+        description: 'Quits Paint.'
+      }
+    ];
+
     return (
       <div className={styles['menu-bar']}>
-        <Menu />
+        <Menu title="File" items={fileItems} />
       </div>
     );
   }
