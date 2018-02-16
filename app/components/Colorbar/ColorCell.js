5f836df41587c5bc97ca8b2fa291b226ea3c979c
wuweiweiwu <wuxx1045@umn.edu>
2018-02-16 01:20:40 -0800

    Got most functionality of the color bar. Optimized updating palette using set interval.

--- app/components/Colorbar/ColorCell.js
+++ app/components/Colorbar/ColorCell.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import uuid from 'uuid/v1';
-import { connect } from 'react-redux';
 
 import styles from './ColorCell.scss';
 
 class ColorCell extends Component {
   componentDidMount() {
+    const { color, onContextMenu, onDoubleClick } = this.props;
+
+    if (this.canvas) {
+      const ctx = this.canvas.getContext('2d');
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, 13, 13);
+    }
+
+    if (this.div) {
+      // right click and control click handler
+      this.div.addEventListener('contextmenu', onContextMenu);
+
+      // double click handler
+      if (onDoubleClick) {
+        this.div.addEventListener('dblclick', onDoubleClick);
+      }
+    }
+  }
+
+  componentDidUpdate() {
     const { color } = this.props;
 
-    if (!this.canvas) return;
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, 13, 13);
+    if (this.canvas) {
+      const ctx = this.canvas.getContext('2d');
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, 13, 13);
+    }
   }
 
   render() {
-    const { style, picker } = this.props;
+    const {
+      style, picker, onClick, color
+    } = this.props;
 
     return (
       <div
         className={classNames({ [styles.picker__cell]: picker, [styles.palette__cell]: !picker })}
         style={style}
+        onClick={onClick}
+        ref={div => (this.div = div)}
       >
         <canvas ref={cv => (this.canvas = cv)} height="13px" width="13px" />
       </div>
--- app/components/Colorbar/Colorbar.js
+++ app/components/Colorbar/Colorbar.js
@@ -1,87 +1,148 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import uuid from 'uuid/v1';
+
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import {
+  changePrimary,
+  changeSecondary,
+  changeTertiary,
+  changePalette,
+  swapPrimarySecondary
+} from '../../actions/color';
 
 import ColorCell from './ColorCell';
 import styles from './Colorbar.scss';
 
 class Colorbar extends Component {
-  componentDidMount() {
-    if (!this.primary) return;
-
-    const ctx1 = this.primary.getContext('2d');
-    ctx1.fillStyle = 'black';
-    ctx1.fillRect(0, 0, 13, 13);
-
-    if (!this.secondary) return;
-    const ctx2 = this.secondary.getContext('2d');
-    ctx2.fillStyle = 'white';
-    ctx2.fillRect(0, 0, 13, 13);
+  componentDidUpdate() {
+    const { tertiary } = this.props;
+    if (this.canvas && tertiary) {
+      const ctx = this.canvas.getContext('2d');
+      ctx.fillStyle = tertiary;
+      ctx.fillRect(0, 0, 29, 29);
+    }
   }
 
   render() {
-    const palette = [
-      '#000000',
-      '#787878',
-      '#790300',
-      '#757A01',
-      '#007902',
-      '#007778',
-      '#0A0078',
-      '#7B0077',
-      '#767A38',
-      '#003637',
-      '#286FFE',
-      '#083178',
-      '#4C00FE',
-      '#783B00',
-      '#FFFFFF',
-      '#BBBBBB',
-      '#FF0E00',
-      '#FAFF08',
-      '#00FF0B',
-      '#00FEFF',
-      '#3400FE',
-      '#FF00FE',
-      '#FBFF7A',
-      '#00FF7B',
-      '#76FEFF',
-      '#8270FE'
-      // '#FF0677',
-      // '#FF7D36'
-    ];
+    const {
+      primary,
+      secondary,
+      palette,
+      changePrimary,
+      changeSecondary,
+      changeTertiary,
+      swapPrimarySecondary,
+      changePalette
+    } = this.props;
 
     return (
       <div className={styles.colorbar}>
         <div className={styles.colorbar__switcher}>
-          <canvas height="29px" width="29px" />
+          <canvas ref={canvas => (this.canvas = canvas)} height="29px" width="29px" />
           <ColorCell
             picker
-            color="white"
+            color={secondary}
             style={{
               position: 'absolute',
               right: '3px',
               bottom: '3px'
             }}
+            onClick={e => {
+              e.preventDefault();
+              swapPrimarySecondary();
+            }}
+            onContextMenu={e => {
+              e.preventDefault();
+              swapPrimarySecondary();
+            }}
           />
           <ColorCell
             picker
-            color="black"
+            color={primary}
             style={{
               position: 'absolute',
               left: '2px',
               top: '4px'
             }}
+            onClick={e => {
+              e.preventDefault();
+              swapPrimarySecondary();
+            }}
+            onContextMenu={e => {
+              e.preventDefault();
+              swapPrimarySecondary();
+            }}
           />
         </div>
         <div className={styles.colorbar__palette}>
-          {palette.map(color => <ColorCell color={color} key={uuid()} />)}
+          {palette.map((color, index) => (
+            <ColorCell
+              key={uuid()}
+              color={color}
+              onClick={e => {
+                e.preventDefault();
+                changePrimary(color);
+              }}
+              onContextMenu={e => {
+                e.preventDefault();
+                if (window.event.ctrlKey) {
+                  changeTertiary(color);
+                } else {
+                  changeSecondary(color);
+                }
+              }}
+              onDoubleClick={e => {
+                e.preventDefault();
+                const colorChanger = document.createElement('input');
+                colorChanger.setAttribute('type', 'color');
+                colorChanger.setAttribute('value', color);
+                colorChanger.click();
+                let timeout;
+                let currColor;
+                colorChanger.addEventListener('change', e => {
+                  clearInterval(timeout);
+                  currColor = e.target.value;
+                  // timeout = setTimeout(() => console.log('change:', currColor), 100);
+                  timeout = setTimeout(() => {
+                    changePalette(index, currColor);
+                    changePrimary(currColor);
+                  }, 100);
+                  // console.log('change:', e.target.value);
+                  // changePalette(index, e.target.value);
+                  // changePrimary(e.target.value);
+                });
+              }}
+            />
+          ))}
         </div>
-        <input ref={input => (this.colorInput = input)} type="color" />
       </div>
     );
   }
 }
 
-export default Colorbar;
+function mapStateToProps(state) {
+  return {
+    primary: state.color.primary,
+    secondary: state.color.secondary,
+    tertiary: state.color.tertiary,
+    palette: state.color.palette
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      changePrimary,
+      changeSecondary,
+      changeTertiary,
+      changePalette,
+      swapPrimarySecondary
+    },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Colorbar);
