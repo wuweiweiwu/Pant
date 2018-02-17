167c86b9fa8365cd6424c3eb11ee975f169e9314
wuweiweiwu <wuxx1045@umn.edu>
2018-02-16 23:37:58 -0800

    Adding all the menu tabs + nested menu

--- app/components/Menubar/Menu.js
+++ app/components/Menubar/Menu.js
@@ -1,56 +1,37 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import uuid from 'uuid/v1';
-import { connect } from 'react-redux';
 
-export const DIVIDER: string = 'DIVIDER';
+import Popup from './Popup';
 
 import styles from './Menu.scss';
 
-type item =
-  | {
-      item: string,
-      shortcut?: string,
-      description: string,
-      disabled?: boolean
-    }
-  | string;
+export function embedHotkey(str: string) {
+  const arr = [];
+  arr.push(str.split('&')[0]);
 
-type Props = {
-  closeMenu: () => void,
-  openMenu: () => void,
-  title: string,
-  activeMenu: boolean,
-  isOpen: boolean,
-  items: Array<item>
-};
+  let hotkey = null;
+  const index = str.indexOf('&');
+  if (index >= 0) {
+    hotkey = str[index + 1];
+  }
+  arr.push(hotkey ? (
+    <span className={styles.menu__hotkey} key={uuid()}>
+      {hotkey}
+    </span>
+  ) : null);
 
-class Menu extends Component<Props> {
+  const second = str.split('&')[1];
+  arr.push(second ? second.substring(1) : null);
+
+  return arr;
+}
+
+class Menu extends Component {
   render() {
     const {
-      title, items, activeMenu, isOpen, openMenu, closeMenu
+      title, items, pressed, isOpen, openMenu, closeMenu
     } = this.props;
-
-    const embedHotkey = (str: string) => {
-      const arr = [];
-      arr.push(str.split('&')[0]);
-
-      let hotkey = null;
-      const index = str.indexOf('&');
-      if (index >= 0) {
-        hotkey = str[index + 1];
-      }
-      arr.push(hotkey ? (
-        <span className={styles.menu__hotkey} key={uuid()}>
-          {hotkey}
-        </span>
-      ) : null);
-
-      const second = str.split('&')[1];
-      arr.push(second ? second.substring(1) : null);
-
-      return arr;
-    };
 
     return (
       <div className={styles.menu__container}>
@@ -60,7 +41,7 @@ class Menu extends Component<Props> {
             [styles['menu__button--active']]: isOpen
           })}
           onMouseOver={() => {
-            if (activeMenu && !isOpen) {
+            if (pressed && !isOpen) {
               openMenu();
             }
           }}
@@ -74,45 +55,10 @@ class Menu extends Component<Props> {
         >
           {embedHotkey(title)}
         </div>
-        <div
-          className={classNames({
-            [styles.menu__popup]: true,
-            [styles['menu__popup--disabled']]: !isOpen
-          })}
-        >
-          <table className={styles.menu__popup__table}>
-            <tbody>
-              {items.map(item => {
-                if (item === DIVIDER) {
-                  return (
-                    <tr key={uuid()}>
-                      <td colSpan={4}>
-                        <hr className={styles.menu__divider} />
-                      </td>
-                    </tr>
-                  );
-                }
-                return (
-                  <tr className={styles.menu__item} key={uuid()} disabled={item.disabled}>
-                    <td className={styles.menu__item__checkbox} />
-                    <td className={styles.menu__item__label}>{embedHotkey(item.item)}</td>
-                    <td className={styles.menu__item__shortcut}>{item.shortcut}</td>
-                    <td className={styles.menu__item__submenu} />
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <Popup items={items} isOpen={isOpen} />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    activeMenu: state.menu.active
-  };
-}
-
-export default connect(mapStateToProps, undefined)(Menu);
+export default Menu;
--- app/components/Menubar/Menubar.js
+++ app/components/Menubar/Menubar.js
@@ -1,194 +1,17 @@
 import React, { Component } from 'react';
-import Menu, { DIVIDER } from './Menu';
+import Menu from './Menu';
 import styles from './Menubar.scss';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { openMenu, closeMenu, FILE, EDIT } from '../../actions/menu';
+import { openMenu, closeMenu, FILE, EDIT, VIEW, IMAGE, COLORS, HELP } from '../../actions/menu';
+
+import { fileItems, editItems, viewItems, imageItems, colorItems, helpItems } from './items';
 
 class MenuBar extends Component {
   render() {
-    const fileItems = [
-      {
-        item: '&New',
-        shortcut: 'Ctrl+N',
-        // action: file_new,
-        description: 'Creates a new document.'
-      },
-      {
-        item: '&Open',
-        shortcut: 'Ctrl+O',
-        // action: file_open,
-        description: 'Opens an existing document.'
-      },
-      {
-        item: '&Save',
-        shortcut: 'Ctrl+S',
-        // action: file_save,
-        description: 'Saves the active document.'
-      },
-      {
-        item: 'Save &As',
-        shortcut: 'Ctrl+Shift+S',
-        // in mspaint, no shortcut is listed, but it supports F12; it doesn't support Ctrl+Shift+S
-        // action: file_save_as,
-        description: 'Saves the active document with a new name.'
-      },
-      DIVIDER,
-      {
-        item: '&Load From URL',
-        // shortcut: "Ctrl+L",
-        // action: file_load_from_url,
-        description: 'Opens an image from the web.'
-      },
-      {
-        item: '&Upload To Imgur',
-        // action: upload_to_imgur,
-        description: 'Uploads the active document to Imgur'
-      },
-      DIVIDER,
-      {
-        item: 'Manage Storage',
-        // action: manage_storage,
-        description: 'Manages storage of previously created or opened pictures.'
-      },
-      DIVIDER,
-      {
-        item: 'Print Pre&view',
-        // action() {
-        //   print();
-        // },
-        description: 'Prints the active document and sets printing options.'
-        // description: "Displays full pages.",
-      },
-      {
-        item: 'Page Se&tup',
-        // action() {
-        //   print();
-        // },
-        description: 'Prints the active document and sets printing options.'
-        // description: "Changes the page layout.",
-      },
-      {
-        item: '&Print',
-        shortcut: 'Ctrl+P',
-        // action() {
-        //   print();
-        // },
-        description: 'Prints the active document and sets printing options.'
-      },
-      DIVIDER,
-      {
-        item: 'Set As &Wallpaper (Tiled)',
-        // action: set_as_wallpaper_tiled,
-        description: 'Tiles this bitmap as the desktop background.'
-      },
-      {
-        item: 'Set As Wallpaper (&Centered)', // in mspaint it's Wa&llpaper
-        // action: set_as_wallpaper_centered,
-        description: 'Centers this bitmap as the desktop background.'
-      },
-      DIVIDER,
-      {
-        item: 'Recent File',
-        disabled: true, // @TODO for chrome app / desktop app
-        description: ''
-      },
-      DIVIDER,
-      {
-        item: 'E&xit',
-        shortcut: 'Alt+F4',
-        // action() {
-        //   close();
-        // },
-        description: 'Quits Paint.'
-      }
-    ];
-
-    const editItems = [
-      {
-        item: '&Undo',
-        shortcut: 'Ctrl+Z',
-        // enabled: function(){
-        // 	return undos.length >= 1;
-        // },
-        // action: undo,
-        description: 'Undoes the last action.'
-      },
-      {
-        item: '&Repeat',
-        shortcut: 'F4',
-        // enabled: function(){
-        // 	return redos.length >= 1;
-        // },
-        // action: redo,
-        description: 'Redoes the previously undone action.'
-      },
-      DIVIDER,
-      {
-        item: 'Cu&t',
-        shortcut: 'Ctrl+X',
-        // enabled: function(){
-        // 	// @TODO disable if no selection (image or text)
-        // 	return (typeof chrome !== "undefined") && chrome.permissions;
-        // },
-        // action: function(){
-        // 	document.execCommand("cut");
-        // },
-        description: 'Cuts the selection and puts it on the Clipboard.'
-      },
-      {
-        item: '&Copy',
-        shortcut: 'Ctrl+C',
-        // enabled: function(){
-        // 	// @TODO disable if no selection (image or text)
-        // 	return (typeof chrome !== "undefined") && chrome.permissions;
-        // },
-        // action: function(){
-        // 	document.execCommand("copy");
-        // },
-        description: 'Copies the selection and puts it on the Clipboard.'
-      },
-      {
-        item: '&Paste',
-        shortcut: 'Ctrl+V',
-        // enabled: function(){
-        // 	return (typeof chrome !== "undefined") && chrome.permissions;
-        // },
-        // action: function(){
-        // 	document.execCommand("paste");
-        // },
-        description: 'Inserts the contents of the Clipboard.'
-      },
-      {
-        item: 'C&lear Selection',
-        shortcut: 'Del',
-        // enabled: function(){ return !!selection; },
-        // action: delete_selection,
-        description: 'Deletes the selection.'
-      },
-      {
-        item: 'Select &All',
-        shortcut: 'Ctrl+A',
-        // action: select_all,
-        description: 'Selects everything.'
-      },
-      DIVIDER,
-      {
-        item: 'C&opy To...',
-        // enabled: function(){ return !!selection; },
-        // action: save_selection_to_file,
-        description: 'Copies the selection to a file.'
-      },
-      {
-        item: 'Paste &From...',
-        // action: paste_from_file_select_dialog,
-        description: 'Pastes a file into the selection.'
-      }
-    ];
-
     const {
-      fileActive, editActive, openMenu, closeMenu
+      active, pressed, openMenu, closeMenu
     } = this.props;
 
     return (
@@ -196,16 +19,50 @@ class MenuBar extends Component {
         <Menu
           title="&File"
           items={fileItems}
-          isOpen={fileActive}
+          isOpen={active === FILE}
           openMenu={() => openMenu(FILE)}
           closeMenu={closeMenu}
+          pressed={pressed}
         />
         <Menu
           title="&Edit"
           items={editItems}
-          isOpen={editActive}
+          isOpen={active === EDIT}
           openMenu={() => openMenu(EDIT)}
           closeMenu={closeMenu}
+          pressed={pressed}
+        />
+        <Menu
+          title="&View"
+          items={viewItems}
+          isOpen={active === VIEW}
+          openMenu={() => openMenu(VIEW)}
+          closeMenu={closeMenu}
+          pressed={pressed}
+        />
+        <Menu
+          title="&Image"
+          items={imageItems}
+          isOpen={active === IMAGE}
+          openMenu={() => openMenu(IMAGE)}
+          closeMenu={closeMenu}
+          pressed={pressed}
+        />
+        <Menu
+          title="&Colors"
+          items={colorItems}
+          isOpen={active === COLORS}
+          openMenu={() => openMenu(COLORS)}
+          closeMenu={closeMenu}
+          pressed={pressed}
+        />
+        <Menu
+          title="&Help"
+          items={helpItems}
+          isOpen={active === HELP}
+          openMenu={() => openMenu(HELP)}
+          closeMenu={closeMenu}
+          pressed={pressed}
         />
       </div>
     );
@@ -214,8 +71,8 @@ class MenuBar extends Component {
 
 function mapStateToProps(state) {
   return {
-    fileActive: state.menu.file,
-    editActive: state.menu.edit
+    pressed: state.menu.pressed,
+    active: state.menu.active
   };
 }
 
