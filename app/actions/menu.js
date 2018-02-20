6d946f26321c8a00c0b1554ae10327ad92748985
wuweiweiwu <wuxx1045@umn.edu>
2018-02-20 00:29:39 -0800

    Reformat menu actions

--- app/actions/menu.js
+++ app/actions/menu.js
@@ -1,5 +1,8 @@
 export const TOGGLE_CHECKBOX = 'TOGGLE_CHECKBOX';
 
 export function toggleCheckbox(target) {
-  return { type: TOGGLE_CHECKBOX, target };
+  return {
+    type: TOGGLE_CHECKBOX,
+    target
+  };
 }
